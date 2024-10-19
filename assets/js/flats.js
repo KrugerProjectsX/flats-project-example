013196df4b5c8010f69cf88cb06411f37784b653
Diego Gaibor <diego.g@scopicsoftware.com>
2024-10-19 12:18:02 -0500

    add favorites

--- assets/js/flats.js
+++ assets/js/flats.js
@@ -1,9 +1,12 @@
 let current_table = [];
 
+
 const submitFlat =  (event) => {
     event.preventDefault();
     const form = event.target;
+    const date = new Date();
     const flat = {
+        id: date.getTime(),
         city: form.elements['city'].value,
         streetName: form.elements['street-name'].value,
         streetNumber: form.elements['street-number'].value,
@@ -37,45 +40,73 @@ const fillTable = (flats = null) =>{
     if (flats!= null && Array.isArray(flats)) {
         flats.forEach((flat)=>{
             const tr = document.createElement('tr');
-            tr.innerHTML = '' +
-                '<td>' + flat.city + '</td>' +
-                '<td>' + flat.streetName + '</td>' +
-                '<td>' + flat.streetNumber + '</td>' +
-                '<td>' + flat.hasAc + '</td>' +
-                '<td>' + flat.areaSize + '</td>' +
-                '<td>' + flat.price + '</td>' +
-                '<td>' + flat.dateAvailable + '</td>' +
-                '<td>' + flat.yearBuilt + '</td>';
-            // const tdCity = document.createElement('td');
-            // tdCity.innerText= flat.city;
-            // tr.appendChild(tdCity);
-            // const tdStreetName = document.createElement('td');
-            // tdStreetName.innerText= flat.streetName;
-            // tr.appendChild(tdStreetName);
-            // const tdStreetNumber = document.createElement('td');
-            // tdStreetNumber.innerText= flat.streetNumber;
-            // tr.appendChild(tdStreetNumber);
-            // const tdHasAc = document.createElement('td');
-            // tdHasAc.innerText= flat.hasAc;
-            // tr.appendChild(tdHasAc);
-            // const tdAreaSize = document.createElement('td');
-            // tdAreaSize.innerText= flat.areaSize;
-            // tr.appendChild(tdAreaSize);
-            // const tdPrice = document.createElement('td');
-            // tdPrice.innerText= flat.price;
-            // tr.appendChild(tdPrice);
-            // const tdDateAvailable = document.createElement('td');
-            // tdDateAvailable.innerText= flat.dateAvailable;
-            // tr.appendChild(tdDateAvailable);
-            // const tdYearBuilt = document.createElement('td');
-            // tdYearBuilt.innerText= flat.yearBuilt;
-            // tr.appendChild(tdYearBuilt);
+            // tr.innerHTML = '' +
+            //     '<td>' + flat.city + '</td>' +
+            //     '<td>' + flat.streetName + '</td>' +
+            //     '<td>' + flat.streetNumber + '</td>' +
+            //     '<td>' + flat.hasAc + '</td>' +
+            //     '<td>' + flat.areaSize + '</td>' +
+            //     '<td>' + flat.price + '</td>' +
+            //     '<td>' + flat.dateAvailable + '</td>' +
+            //     '<td>' + flat.yearBuilt + '</td>' + 
+            //     '<td>' + '<button onclick="addFavorite('+flat.id+')">Add</button>'+ '</td>';
+            const tdCity = document.createElement('td');
+            tdCity.innerText= flat.city;
+            tr.appendChild(tdCity);
+            const tdStreetName = document.createElement('td');
+            tdStreetName.innerText= flat.streetName;
+            tr.appendChild(tdStreetName);
+            const tdStreetNumber = document.createElement('td');
+            tdStreetNumber.innerText= flat.streetNumber;
+            tr.appendChild(tdStreetNumber);
+            const tdHasAc = document.createElement('td');
+            tdHasAc.innerText= flat.hasAc;
+            tr.appendChild(tdHasAc);
+            const tdAreaSize = document.createElement('td');
+            tdAreaSize.innerText= flat.areaSize;
+            tr.appendChild(tdAreaSize);
+            const tdPrice = document.createElement('td');
+            tdPrice.innerText= flat.price;
+            tr.appendChild(tdPrice);
+            const tdDateAvailable = document.createElement('td');
+            tdDateAvailable.innerText= flat.dateAvailable;
+            tr.appendChild(tdDateAvailable);
+            const tdYearBuilt = document.createElement('td');
+            tdYearBuilt.innerText= flat.yearBuilt;
+            tr.appendChild(tdYearBuilt);
+            
+            const tdAdd = document.createElement('td');
+            
+            const button = document.createElement('button');
+            button.onclick = (e) => toggleFavorite(flat.id,e);
+            button.innerText = (checkFlatFavorite(flat.id))?'Remove':'Add'
+            
+            tdAdd.appendChild(button);
+            
+            tr.appendChild(tdAdd);
+            
             tbody.appendChild(tr);
         });
     }
     
     
 }
+
+const checkFlatFavorite = (id) => {
+    const flats_favorites = JSON.parse(localStorage.getItem('flats_favorites'));
+    if (flats_favorites != null) {
+        const exist = flats_favorites.findIndex((item)=>{
+            return item.idFlat === id
+        })
+        if (exist !== -1){
+            return true;
+        }
+        
+    }
+    return false;
+}
+
+
 
 const filterTable = (event) => {
     event.preventDefault();
@@ -145,6 +176,40 @@ const orderTable =(column)=>{
     
     
 } 
+
+const toggleFavorite = (id,e) => {
+    const user_logged = JSON.parse(localStorage.getItem('user_logged'));
+    if (!user_logged){
+        alert('You must be logged in to add a favorite');
+        return;
+    }
+    
+    const email = user_logged.email
+    
+    const favorite = {
+        idFlat:id,
+        emailUser:email
+    }
+    const flats_favorites = JSON.parse(localStorage.getItem('flats_favorites'));
+    if (flats_favorites == null) {
+        const flats_favorites = [favorite];
+        localStorage.setItem('flats_favorites', JSON.stringify(flats_favorites));
+    }else{
+        const exist =  flats_favorites.findIndex((item)=>{
+            return item.idFlat === id
+        })
+        if (exist === -1){
+            flats_favorites.push(favorite);
+            e.target.textContent = 'Remove';
+        }else{
+            flats_favorites.splice(exist, 1);
+            e.target.textContent = 'Add';
+        }
+       
+        localStorage.setItem('flats_favorites', JSON.stringify(flats_favorites));
+    }
+}
+
 
 document.addEventListener('DOMContentLoaded', () => {
     fillTable();
--- assets/js/home.js
+++ assets/js/home.js
@@ -4,4 +4,77 @@ document.addEventListener('DOMContentLoaded', function() {
   if (user_logged == null) {
       window.location.href = 'index.html'
   }
+  
+  const fillTable = () =>{
+      
+      const favorites = JSON.parse(localStorage.getItem('flats_favorites'));
+      const flats = JSON.parse(localStorage.getItem('flats'));
+      const tbody = document.querySelector('#flats-table tbody');
+      tbody.innerHTML = '';
+      
+      if (favorites && Array.isArray(favorites)){ 
+          const filtered = favorites.filter((item)=>{
+              return  item.emailUser === user_logged.email
+          
+          })
+          const flatsTable = filtered.map( (item)=>{
+              const id = item.idFlat;
+              return flats.find((flat)=> {
+                  return flat.id === id
+              })
+          })
+          if (flatsTable!= null && Array.isArray(flatsTable)) {
+              flatsTable.forEach((flat)=>{
+                  const tr = document.createElement('tr');
+                  // tr.innerHTML = '' +
+                  //     '<td>' + flat.city + '</td>' +
+                  //     '<td>' + flat.streetName + '</td>' +
+                  //     '<td>' + flat.streetNumber + '</td>' +
+                  //     '<td>' + flat.hasAc + '</td>' +
+                  //     '<td>' + flat.areaSize + '</td>' +
+                  //     '<td>' + flat.price + '</td>' +
+                  //     '<td>' + flat.dateAvailable + '</td>' +
+                  //     '<td>' + flat.yearBuilt + '</td>' + 
+                  //     '<td>' + '<button onclick="addFavorite('+flat.id+')">Add</button>'+ '</td>';
+                  const tdCity = document.createElement('td');
+                  tdCity.innerText= flat.city;
+                  tr.appendChild(tdCity);
+                  const tdStreetName = document.createElement('td');
+                  tdStreetName.innerText= flat.streetName;
+                  tr.appendChild(tdStreetName);
+                  const tdStreetNumber = document.createElement('td');
+                  tdStreetNumber.innerText= flat.streetNumber;
+                  tr.appendChild(tdStreetNumber);
+                  const tdHasAc = document.createElement('td');
+                  tdHasAc.innerText= flat.hasAc;
+                  tr.appendChild(tdHasAc);
+                  const tdAreaSize = document.createElement('td');
+                  tdAreaSize.innerText= flat.areaSize;
+                  tr.appendChild(tdAreaSize);
+                  const tdPrice = document.createElement('td');
+                  tdPrice.innerText= flat.price;
+                  tr.appendChild(tdPrice);
+                  const tdDateAvailable = document.createElement('td');
+                  tdDateAvailable.innerText= flat.dateAvailable;
+                  tr.appendChild(tdDateAvailable);
+                  const tdYearBuilt = document.createElement('td');
+                  tdYearBuilt.innerText= flat.yearBuilt;
+                  tr.appendChild(tdYearBuilt);
+
+                  tbody.appendChild(tr);
+              });
+          }
+              
+          
+      }
+     
+      
+      
+  }
+    fillTable();    
+  
 })
+
+
+
+
